5d01ea9bc32c4341e96d5a1fe650866f6a8c804f
Lian <weijhih.a.lian@rakuten.com>
2020-11-22 15:31:42 +0900

    Initial React application

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    semi: 0,
+    'no-unused-vars': [
+      1,
+      {
+        vars: 'all',
+        args: 'after-used',
+      },
+    ],
+    'no-shadow': 0,
+    'no-unused-expressions': 0,
+    'no-useless-escape': 2,
+    'react/no-array-index-key': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./client/index.js",
+  devServer: {
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./client/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
