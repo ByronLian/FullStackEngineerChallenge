fcdc9988287042112ac1359cd3471a63019fd988
Lian <weijhih.a.lian@rakuten.com>
2020-11-22 15:47:48 +0900

    Add react-router-dom for Routering

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/index'
+import PageNotFound from './pages/404/index'
 import './App.css'
 
 const App = () => {
-  return <>Hi </>
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={PageNotFound} />
+    </Switch>
+  )
 }
 
 export default App
